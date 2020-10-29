1871c56cbd0b6e20e20674b83af9a6cef628766a
codegangsk <kjhj0604@gmail.com>
2020-10-29 11:02:34 +0900

    enable button's background shadow to appear/disappear when tapped

--- 5.3MusicWireFrame/ViewController.swift
+++ 5.3MusicWireFrame/ViewController.swift
@@ -31,15 +31,15 @@ extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        reverseButtonBackground.layer.cornerRadius = 35.0
+        reverseButtonBackground.layer.cornerRadius = 30.0
         reverseButtonBackground.clipsToBounds = true
         reverseButtonBackground.alpha = 0.0
         
-        playPauseButtonBackground.layer.cornerRadius = 35.0
+        playPauseButtonBackground.layer.cornerRadius = 30.0
         playPauseButtonBackground.clipsToBounds = true
         playPauseButtonBackground.alpha = 0.0
         
-        forwardButtonBackground.layer.cornerRadius = 35.0
+        forwardButtonBackground.layer.cornerRadius = 30.0
         forwardButtonBackground.clipsToBounds = true
         forwardButtonBackground.alpha = 0.0 
     }
@@ -57,6 +57,49 @@ extension ViewController {
             })
         }
         isPlaying.toggle()
+    }
+    
+    @IBAction func touchDown(_ sender: UIButton) {
+        let buttonBackground: UIView
+        
+        switch sender {
+        case reverseButton:
+            buttonBackground = reverseButtonBackground
+        case playPauseButton:
+            buttonBackground = playPauseButtonBackground
+        case forwardButton:
+             buttonBackground = forwardButtonBackground
+        default:
+            return
+        }
+        
+        UIView.animate(withDuration: 0.25) {
+            buttonBackground.alpha = 0.3
+            sender.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }
+    }
+    
+    @IBAction func touchUpInside(_ sender: UIButton) {
+        var buttonBackground: UIView
+        
+        switch sender {
+        case reverseButton:
+            buttonBackground = reverseButtonBackground
+        case playPauseButton:
+            buttonBackground = playPauseButtonBackground
+        case forwardButton:
+            buttonBackground = forwardButtonBackground
+        default:
+            return
+        }
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            buttonBackground.alpha = 0.0
+            buttonBackground.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            sender.transform = CGAffineTransform.identity
+        }) { (_) in
+            buttonBackground.transform = CGAffineTransform.identity
+        }
     }
 }
 
